643cd3fb16c4917c82c6cbae15ecb141d8d6b728
jch <ggongco1120@gmail.com>
2025-05-08 14:18:01 +0900

    0508js/오전수업/형변환,연산자,2진수,8진수,16진수

---
+++ js_0508/js/01.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+    let num1 = 55;          // 10진수 : (0-9)
+    let num2 = 0B110111;    // 2진수  : binary(0,1)
+    let num3 = 0x37;        // 16진수 : heXa(a-f)-decimal(0-9)
+    let num4 = 0o67;        // 8진수  : octal
+
+    console.log("num1 : ",num1);
+    console.log("num2 : ",num2);
+    console.log("num3 : ",num3);
+    console.log("num4 : ",num4);
+
+};
